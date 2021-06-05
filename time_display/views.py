498a948ff53c2cd6233ed2bea8f88825838dba8b
Sergio Ananias <sergio.ananias.o@gmail.com>
2021-06-05 14:10:09 -0400

    Se ajusta a hora local

--- time_display/views.py
+++ time_display/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime
 # Create your views here.
 
 def index(request):
     return redirect('/time_display')
 
 def time_display(request):
+    date_time = localtime()
     context = {
-    "hour": strftime("%H:%M %p", gmtime()),
-    "date": strftime("%Y-%m-%d" ,gmtime())
+    "hour": date_time.tm_hour,
+    "min": date_time.tm_min,
+    "sec": date_time.tm_sec,
+    "day": date_time.tm_mday,
+    "month": date_time.tm_mon,
+    "year": date_time.tm_year
     }
     return render(request,'index.html', context)
